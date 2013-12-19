89823af8d2719b2f45b11e9944251fc3be5585c3
Yong-Soo Chung <yongsoochung@gmail.com>
2013-12-19 15:18:54 -0800

    Created skeleton for parkerListController, parkerPickUpController, list.html, pickUp.html

--- public/client/controllers/parkerListController.js
+++ public/client/controllers/parkerListController.js
@@ -1,4 +1,14 @@
 angular.module('appModule')
-.controller('parkerListController', function($scope){
+.controller('parkerListController', function($scope, riderFactory){
+	$scope.riders = [
+		{ name: "John Smith", profilePhoto: "something.jpg", carLocation: 23294 },
+		{ name: "Bill Smith", profilePhoto: "anotherThing.jpg", carLocation: 23544 },
+		{ name: "Tim Smith", profilePhoto: "yetAnother.jpg", carLocation: 635294 },
+		{ name: "Ron Smith", profilePhoto: "anotherYetAnother.jpg", carLocation: 53381 }
+	];
 
-})
+	$scope.selectRider = function(rider) {
+		riderFactory.rider = rider;
+	};
+
+});
