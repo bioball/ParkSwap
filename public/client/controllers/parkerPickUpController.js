0b3d2f32fa928e72c2e46cc3b2802db28cccf481
Yong-Soo Chung <yongsoochung@gmail.com>
2013-12-19 20:58:12 -0800

    Added pingServer function to parkerListController

--- public/client/controllers/parkerPickUpController.js
+++ public/client/controllers/parkerPickUpController.js
@@ -1,9 +1,9 @@
 angular.module('appModule')
-.controller('parkerPickUpController', function($scope, $http, parkerServices){
+.controller('parkerPickUpController', function($scope, parkerServices){
 	$scope.rider = parkerServices.getRider();
 
 	$scope.callRider = function() {
-		
+
 	};
 
 	$scope.navigate = function() {
