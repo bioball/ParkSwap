bfd3763773a922a41b3f49b625380ef8425bcc22
Yong-Soo Chung <yongsoochung@gmail.com>
2013-12-19 20:06:58 -0800

    Added  to parkerPickUpController

--- public/client/controllers/parkerPickUpController.js
+++ public/client/controllers/parkerPickUpController.js
@@ -1,9 +1,9 @@
 angular.module('appModule')
-.controller('parkerPickUpController', function($scope, parkerServices){
+.controller('parkerPickUpController', function($scope, $http, parkerServices){
 	$scope.rider = parkerServices.getRider();
 
 	$scope.callRider = function() {
-
+		
 	};
 
 	$scope.navigate = function() {
