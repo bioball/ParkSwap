1d0a576fd394b261d1b1a6aacdd919cb3b5c55f0
Daniel Chao <daniel.h.chao@gmail.com>
2013-12-19 16:33:33 -0800

    more work done on rider wait and where

--- public/client/services/riderServices.js
+++ public/client/services/riderServices.js
@@ -2,14 +2,19 @@ angular.module('appModule')
 .factory('riderServices', function($q, $http){
   return {
     cancel: function(){
-      var 
+      var deferred = $q.defer();
       $http({
         method: 'POST',
         url: 'riders/cancel',
         data: {
-          rider: "this rider";
+          rider: "this rider"
         }
-      })
+      }).success(function(data){
+        deferred.resolve(data)
+      }).error(function(err){
+        deferred.reject(err)
+      });
+      return deferred.promise;
     },
     setParker: function(parker){
       this.parker = parker;
