b63379a9ad086b9e3131cffb06fb4b86ae05eed4
Daniel Chao <daniel.h.chao@gmail.com>
2013-12-30 10:46:20 -0800

    static assets moved to routes.js

--- app/main.js
+++ app/main.js
@@ -4,7 +4,6 @@ var path    = require('path');
 
 require('./config/config.js')(app);
 require('./config/routes.js')(app);
-app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // development only
 if ('development' == app.get('env')) {
