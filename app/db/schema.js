02008f23c762b8bbbd1b119e8c8c7d2b345d8d77
Daniel Chao <daniel.h.chao@gmail.com>
2014-01-08 20:13:30 -0800

    add id field in schema, and use integers for number fields

--- app/db/schema.js
+++ app/db/schema.js
@@ -2,11 +2,13 @@ var DB = require("./init.js");
 
 DB.knex.schema.createTable('users', 
   function(table){
-    table.string('uid');
+    table.increments('id').primary();
+    table.integer('uid');
     table.string('first_name');
     table.string('last_name');
-    table.string('phone');
-    table.string('name')
+    table.integer('phone');
+    table.string('name');
+    table.timestamps();
   }).then(function(){
   console.log("User schema created successfully...");
 });
