24a64203fb390f4a259661d247973ed9ae288522
Daniel Chao <daniel.h.chao@gmail.com>
2014-01-08 20:16:31 -0800

    string for all columns in schema

--- app/db/schema.js
+++ app/db/schema.js
@@ -3,10 +3,10 @@ var DB = require("./init.js");
 DB.knex.schema.createTable('users', 
   function(table){
     table.increments('id').primary();
-    table.integer('uid');
+    table.string('uid');
     table.string('first_name');
     table.string('last_name');
-    table.integer('phone');
+    table.string('phone');
     table.string('name');
     table.timestamps();
   }).then(function(){
