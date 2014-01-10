a24838b0d8afdecaae13691ad24fadc08208ad85
Yong-Soo Chung <yongsoochung@gmail.com>
2014-01-09 19:59:55 -0800

    map now adjusts to rider's current location and map is draggable

--- public/client/controllers/riderWhereController.js
+++ public/client/controllers/riderWhereController.js
@@ -4,8 +4,29 @@ angular.module('appModule')
     riderLocation = {
       lat: position.coords.latitude,
       lng: position.coords.longitude
-    };    
+    };
+
+    var markerArr = [];
+
+    $scope.myMap.setCenter(new google.maps.LatLng(riderLocation.lat, riderLocation.lng));
+    var firstMarker = $scope.addMarkerToMap($scope.myMap.getCenter());
+    markerArr.push(firstMarker);
+
+    google.maps.event.addListener($scope.myMap, 'drag', function() {
+      for (var i = 0; i < markerArr.length; i++) {
+        markerArr[i].setMap(null);
+      }
+      var marker = $scope.createMarker($scope.myMap.getCenter().b, $scope.myMap.getCenter().d);
+      markerArr.push(marker);
+    });
+
+    google.maps.event.addListener($scope.myMap, 'dragend', function() {
+      geocodeServices.getAddress({ lat: $scope.myMap.getCenter().b, lng: $scope.myMap.getCenter().d }).then(function(address){
+        $scope.carLocation = address;
+      });
+    });
   });
+
   $scope.submitRider = function(){
     geocodeServices.getCoords($scope.carLocation)
     .then(function(carLocation){
@@ -36,12 +57,12 @@ angular.module('appModule')
   }
 
   $scope.mapOptions = {
-    center: new google.maps.LatLng(37.7749295, -122.4194155),
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: new google.maps.LatLng(37.783648, -122.409173799999),
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
 
-  var findMarkerAddress = function(evt) {
+  $scope.findMarkerAddress = function(evt) {
      var coord = {};
      coord.lat = evt.latLng.lat().toFixed(3);
      coord.lng = evt.latLng.lng().toFixed(3);
@@ -53,49 +74,43 @@ angular.module('appModule')
   };
 
   $scope.addMarkerToMap = function(latLng) {
-
-    if ($scope.markerOnMap !== undefined) {
-      $scope.markerOnMap.setMap(null);
-
-    }
     
     $scope.markerOnMap = new google.maps.Marker({
       map: $scope.myMap,
       position: latLng,
-      draggable:true,
-      animation: google.maps.Animation.DROP,
+      draggable: false,
+      // animation: google.maps.Animation.DROP,
     });
 
+    // google.maps.event.addListener($scope.markerOnMap, 'dragend', $scope.findMarkerAddress);
+    // google.maps.event.addListener($scope.markerOnMap, 'drag', $scope.findMarkerAddress);
 
-    google.maps.event.addListener($scope.markerOnMap, 'dragend', findMarkerAddress);
-    google.maps.event.addListener($scope.markerOnMap, 'drag', findMarkerAddress);
-
-    $scope.myMap.setZoom(15);
+    // $scope.myMap.setZoom(15);
     $scope.myMap.panTo($scope.markerOnMap.position);
+    return $scope.markerOnMap;
   };
   
   $scope.createMarker = function(lat, lng) {
     var latLng = new google.maps.LatLng(lat, lng);
-    $scope.addMarkerToMap(latLng);
+    return $scope.addMarkerToMap(latLng);
   };
 
-  $scope.addMarker = function($event, $params) {
-    $scope.addMarkerToMap($params[0].latLng);
-    var coord = {};
-    coord.lat = $params[0].latLng.lat();
-    coord.lng = $params[0].latLng.lng();
-    geocodeServices.getAddress(coord)
-    .then(function(address) {
-      $scope.carLocation = address;
-    })
-
-  };    
+  // $scope.addMarker = function($event, $params) {
+  //   $scope.addMarkerToMap($params[0].latLng);
+  //   var coord = {};
+  //   coord.lat = $params[0].latLng.lat();
+  //   coord.lng = $params[0].latLng.lng();
+  //   geocodeServices.getAddress(coord)
+  //   .then(function(address) {
+  //     $scope.carLocation = address;
+  //   })
+  // };    
 
   $scope.showCarLocation = function () {
     geocodeServices.getCoords($scope.carLocation)
     .then(function(carLocation) {
       $scope.createMarker(carLocation.lat, carLocation.lng);
-    })    
+    })
   };
 
   $scope.home = function(){
